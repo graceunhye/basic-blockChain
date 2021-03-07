eaccbc6623afa126b9a0360af8876797cb714812
graceunhye <9rac.eun@gmail.com>
2021-03-07 21:43:59 +0900

    Add Block

--- src/eunhyeChain/Block.java
+++ src/eunhyeChain/Block.java
@@ -1,5 +1,18 @@
 package eunhyeChain;
 
-public class Block {
+import java.util.Date;
 
+public class Block {
+	
+	public String hash;
+	public String previousHash;
+	public String data;
+	public long timestamp;
+	
+	//Block Constructor.
+	public Block(String data, String previousHash) {
+		this.data = data;
+		this.previousHash = previousHash;
+		this.timestamp = new Date().getTime();
+	}
 }
